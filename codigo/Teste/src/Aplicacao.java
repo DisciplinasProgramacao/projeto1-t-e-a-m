6fad563f25362b2e5a45dd7bf957acc83f90c5a0
André <afmendes@sga.pucminas.br>
2022-09-10 00:09:15 -0300

    Correção da sequencia do compromisso recorrente

--- codigo/Teste/src/Aplicacao.java
+++ codigo/Teste/src/Aplicacao.java
@@ -6,8 +6,8 @@ public class Aplicacao {
 		System.out.println("1. Incluir compromisso na agenda");
 		System.out.println("2. Remover compromisso da agenda");
 		System.out.println("3. Consultar compromissos agendados");
-		System.out.println("4. Finalizar a agenda");
-		System.out.println("5. Incluir compromisso recorrente");
+		System.out.println("4. Incluir compromisso recorrente");
+		System.out.println("5. Finalizar a agenda");
 		System.out.println("Digite a opcao desejada:");
 	}
 
@@ -18,24 +18,24 @@ public class Aplicacao {
 		String data, tarefa, num, strRepeticoes;
 		Integer i = 0, repeticoes;
 		Boolean isAtivo = true;
-		
+
 		minhaLista = new ListaEncadeada();
-		
+
 		try (Scanner ler = new Scanner(System.in)) {
-			while(isAtivo){
+			while (isAtivo) {
 				menu();
 
 				num = ler.nextLine();
-				
-				switch(Integer.parseInt(num)) {
-				
+
+				switch (Integer.parseInt(num)) {
+
 				case 1:
 					System.out.println("Informe o dia, mês e ano separados por '/'");
 					data = ler.nextLine();
-					
+
 					System.out.println("Informe a atividade");
 					tarefa = ler.nextLine();
-					
+
 					novo = new Compromisso(data, tarefa);
 					try {
 						minhaLista.inserir(novo, i);
@@ -44,7 +44,7 @@ public class Aplicacao {
 					}
 					i++;
 					break;
-					
+
 				case 2:
 					try {
 						removido = minhaLista.remover(0);
@@ -54,7 +54,7 @@ public class Aplicacao {
 						System.out.println(erro.getMessage());
 					}
 					break;
-					
+
 				case 3:
 					try {
 						minhaLista.imprimir();
@@ -62,56 +62,56 @@ public class Aplicacao {
 						System.out.println(erro.getMessage());
 					}
 					break;
-					
 				case 4:
-					isAtivo = false;
-					System.out.println("Finalizado!");
-					break;
-					
-				case 5:
 					System.out.println("Informe o dia, mês e ano separados por '/'");
 					data = ler.nextLine();
-					
+
 					System.out.println("Informe a atividade");
 					tarefa = ler.nextLine();
-					
+
 					System.out.println("Informe quantas vezes esta tarefa será repetida nas próximas semanas");
 					strRepeticoes = ler.nextLine();
 					repeticoes = Integer.parseInt(strRepeticoes);
-					
-					
+
 					int dia, mes, ano;
-					    	
+
 					String partes[] = data.split("/");
 					dia = Integer.parseInt(partes[0]);
 					mes = Integer.parseInt(partes[1]);
 					ano = Integer.parseInt(partes[2]);
-				    	
+
 					Data dataReal = new Data(dia, mes, ano);
-						
-					
-					for(int j=0; j<repeticoes; j++) {
+
+					for (int j = 0; j < repeticoes; j++) {
 						novo = new Compromisso(data, tarefa);
-						
+
 						data = dataReal.pularDias(dataReal, 7);
-				    	
+
 						try {
 							minhaLista.inserir(novo, i);
 						} catch (Exception erro) {
 							System.out.println(erro.getMessage());
 						}
+						i++;
 					}
-					i++;
+
 					break;
-					
+
+				case 5:
+					isAtivo = false;
+					System.out.println("Finalizado!");
+					break;
+
+				
+
 				default:
 					System.out.println("Comando inválido");
 					break;
-						
+
 				}
-				
+
 			}
 		}
-		
+
 	}
 }
